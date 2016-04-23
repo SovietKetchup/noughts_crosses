f760ade9ee5cc0377ca80d00783fe53bd387fa74
SovietKetchup <sam.comer+git@gmail.com>
2016-04-23 19:51:05 +0100

    Added win logic and game works so far

--- noughts_crosses.rb
+++ noughts_crosses.rb
@@ -1,6 +1,6 @@
 # Noughts and Crosses
 # SovietKetchup
-# v0.5.3
+# v1.0.0
 
 # The board the game is played on
 class Board
@@ -29,15 +29,43 @@ class Player
   end
 
   # Check if player has won
-  def won?
+  def won? board
+    # Current board positions
+    a = board.instance_variable_get("@a")
+    b = board.instance_variable_get("@b")
+    c = board.instance_variable_get("@c")
+    d = board.instance_variable_get("@d")
+    e = board.instance_variable_get("@e")
+    f = board.instance_variable_get("@f")
+    g = board.instance_variable_get("@g")
+    h = board.instance_variable_get("@h")
+    i = board.instance_variable_get("@i")
+
+    # Winning combinations
+    if a == @side
+      if b == @side && c == @side; true
+      elsif d == @side && g == @side; true
+      elsif e == @side && i == @side; true
+      end
+    elsif b == @side && e == @side && h == @side; true
+    elsif c == @side
+      if e == @side && g == @side; true
+      elsif f == @side && i == @side; true
+      end
+    elsif d == @side && e == @side && f == @side; true
+    elsif g == @side && h == @side && i == @side; true
+    else; false
+    end
   end
 
 end
 
 board = Board.new
+p1 = Player.new "X"
+p2 = Player.new "O"
 
 # Functions for turn taking logic
-def game board
+def game board, p1, p2
 
   player = "X"
   rounds = 0
@@ -67,19 +95,26 @@ def game board
     # Make turn
     board.place choice, player
 
-    # Change player
+    # Check for a win
+    if player == "X" && p1.won?(board) == true
+      a_win = true
+      winner = "Xs Won"
+    elsif player == "O" && p2.won?(board) == true
+      a_win = true
+      winner = "Os Won"
+    end
+
+    # Change player and increment counter
     if player == "X" then player = "O"
     else player = "X" end
-
-    # Increment round counter
     rounds += 1
-
   end
 
   puts "\n\nGAME OVER :: #{winner}"
-
+  # Draw the final board
+  board.draw
 end
 
 # Run the game
 puts "The game will start with Xs"
-game board
+game board, p1, p2
